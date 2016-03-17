a1d7ce2a0153d00c97d546896eeef902e29e009d
Mohammad Humaid <mohammad.humaid@kcl.ac.uk>
2016-03-17 17:24:44 +0000

    UserSetting UI is complete.

--- app/src/main/java/com/example/mikey/database/UserProfile/UserSettings.java
+++ app/src/main/java/com/example/mikey/database/UserProfile/UserSettings.java
@@ -13,6 +13,10 @@ public class UserSettings extends AppCompatActivity {
 
     private Button btnEditUserProfile;
     private Button btnEditUserInterests;
+    private Button btnBlockContact;
+    private Button btnLogOut;
+    private Button btnDeleteAccount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,16 +43,66 @@ public class UserSettings extends AppCompatActivity {
             }
         });
 
-        //TODO delete this method
-        // testing if stuff is stored
-        final Button test = (Button)findViewById(R.id.btnTest);
-        test.setOnClickListener(new View.OnClickListener() {
+        //start activity which will show blocked contacts
+        btnBlockContact = (Button)findViewById(R.id.btnBlockContact);
+        btnBlockContact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(UserSettings.this, testActivity.class);
-                startActivity(intent);
+
             }
         });
+
+        //start activity which will show blocked contacts
+        btnBlockContact = (Button)findViewById(R.id.btnBlockContact);
+        btnBlockContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //TODO intent for blocked activity
+
+            }
+        });
+
+        // log out of the profile and redirect to log in screen
+        btnLogOut = (Button)findViewById(R.id.btnLogOut);
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // TODO log out of profile and redirect to login screen
+                Intent intent = new Intent(UserSettings.this, Login.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        //
+        btnDeleteAccount = (Button)findViewById(R.id.btnDeleteAccount);
+        btnDeleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // TODO ask the user to enter their password, confirm they want to delete account,
+                // TODO then use the appropriate SQL command to delete and redirecet to login activity.
+                Intent intent = new Intent(UserSettings.this, Login.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        });
+
+//        //TODO delete this method
+//        // testing if stuff is stored
+//        final Button test = (Button)findViewById(R.id.btnTest);
+//        test.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(UserSettings.this, testActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
     }
 
--- app/src/main/java/com/example/mikey/database/UserProfile/testActivity.java
+++ app/src/main/java/com/example/mikey/database/UserProfile/testActivity.java
@@ -8,12 +8,13 @@ import com.example.mikey.database.R;
 
 public class testActivity extends AppCompatActivity {
 
+    // TODO DELETE THIS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         UserInterests interests = new UserInterests();
         TextView test = (TextView)findViewById(R.id.testText);
-        test.setText("TEST " + interests.getInterests("avatar"));
+        test.setText("TEST " + interests.getInterests("biography"));
     }
 }
